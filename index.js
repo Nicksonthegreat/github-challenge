a1fc3c0bb826661a83fc9f9d4995a549cf7aa38b
Nicksonthegreat <138101503+Nicksonthegreat@users.noreply.github.com>
2023-07-08 12:37:20 +0430

    updated the time

--- index.js
+++ index.js
@@ -10,6 +10,28 @@ function searchCity(event) {
 let searchForm = document.querySelector("#search-engine");
 searchForm.addEventListener("submit", searchCity);
 
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day}, ${hours}:${minutes}`;
+}
 function showTemperature(response) {
   let temperature = document.querySelector("#temperature");
   let weatherDescription = document.querySelector(
@@ -17,10 +39,12 @@ function showTemperature(response) {
   );
   let humidity = document.querySelector("#humidity");
   let wind = document.querySelector("#wind");
+  let dateElement = document.querySelector("#current-date");
   temperature.innerHTML = Math.round(response.data.main.temp);
   weatherDescription.innerHTML = response.data.weather[0].description;
   humidity.innerHTML = response.data.main.humidity;
   wind.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 function showCurrentTemperature(response) {
   let temperature = document.querySelector("#temperature");
